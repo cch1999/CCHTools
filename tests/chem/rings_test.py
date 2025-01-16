6847ab8df01bca686b5573d2c0861cb16edb407b
Charlie Harris <charlieharris001@gmail.com>
2025-01-16 09:51:42 +0000

    fix tests

--- tests/chem/rings_test.py
+++ tests/chem/rings_test.py
@@ -5,18 +5,6 @@ from cchtools.chem.rings import generate_ring_systems
 from cchtools.data.constants import RING_ATOMS, RING_SKELETONS
 
 
-@pytest.fixture(scope="module")
-def ring_mols():
-    return {k: Chem.MolFromSmiles(v) for k, v in RING_SKELETONS.items()}
-
-
-def test_ring_skeletons():
-    ring_mols = {k: Chem.MolFromSmiles(v) for k, v in RING_SKELETONS.items()}
-
-
-def test_ring_atoms():
-    atom_mols = {k: Chem.MolFromSmiles(v) for k, v in RING_ATOMS.items()}
-
 
 def test_generate_ring_systems():
     reduced_ring_skeletons = {
